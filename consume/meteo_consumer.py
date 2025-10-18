6441ee64a12405b3bc153e5df87bc668e7be8367
Arthur <arthur.grevin@gmail.com>
2025-10-18 23:51:03 +0200

    Add Linter with ruff and pre-commit

--- consume/meteo_consumer.py
+++ consume/meteo_consumer.py
@@ -2,14 +2,19 @@ from kafka import KafkaConsumer
 import json
 
 class MeteoKafkaConsumer:
-    def __init__(self, kafka_broker, topic, group_id="meteo_consumer_group", auto_offset_reset="earliest"):
+    def __init__(self,
+                 kafka_broker,
+                 topic,
+                 group_id="meteo_consumer_group",
+                 auto_offset_reset="earliest"):
         """
         Initialise le consumer Kafka pour les données météo.
 
         :param kafka_broker: adresse du broker Kafka (ex: 'localhost:9092')
         :param topic: topic Kafka à consommer
         :param group_id: ID du groupe de consommateurs Kafka
-        :param auto_offset_reset: 'earliest' pour lire depuis le début ou 'latest' pour ne lire que les nouveaux messages
+        :param auto_offset_reset: 'earliest'
+        pour lire depuis le début ou 'latest' pour ne lire que les nouveaux messages
         """
         self.kafka_broker = kafka_broker
         self.topic = topic
