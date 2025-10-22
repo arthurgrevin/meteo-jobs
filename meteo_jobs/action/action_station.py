40d3545d71cbc7463382c1929c7cd7f16af5c53d
Arthur <arthur.grevin@gmail.com>
2025-10-22 20:58:03 +0200

    Add Station Extract and Load
    Create Action structure to lazy process station one by one

---
+++ meteo_jobs/action/action_station.py
@@ -0,0 +1,20 @@
+from .action import Action
+from typing import Iterator
+from meteo_jobs.models import Station
+import subprocess
+
+
+
+class ActionStation(Action):
+
+    def __init__(self):
+        """"""
+
+    def __launch_meteo_extract():
+        """Launch a process to start extract meteo for one station"""
+
+
+    def execute(self, records: Iterator[Station]) -> Iterator[Station]:
+        for station in records:
+            print(f"execute for {station.id_nom}")
+            yield station
