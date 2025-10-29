452d3cd8e265141c3a3b26966c85b9ad4dcb7c2e
Arthur <arthur.grevin@gmail.com>
2025-10-29 23:01:50 +0100

    [IMP] Improve definition of job

--- meteo_jobs/models/job.py
+++ meteo_jobs/models/job.py
@@ -5,4 +5,7 @@ class Job:
     job_id: int | None
     job_name: str
     table_name: str
+    load_connector: str
+    extract_connector: str
+    options: dict
     last_compute: str
--- tests/job_postgres_loader_test.py
+++ tests/job_postgres_loader_test.py
@@ -4,6 +4,7 @@ from meteo_jobs.models import Job
 from meteo_jobs.logger import get_logger
 from meteo_jobs.extract import Extract
 import pytest
+import copy
 from datetime import date
 
 logger = get_logger(__name__)
@@ -25,6 +26,9 @@ job = Job(
         job_id=None,
         job_name="test_job",
         table_name="table_test",
+        load_connector="load_test",
+        extract_connector="extract_test",
+        options= {"test":"test"},
         last_compute=date.today().strftime("%Y-%m-%d, %H:%M:%S")
     )
 
@@ -48,28 +52,31 @@ def test_load_job():
     job_read = records[0]
     assert job_read[1] == job.job_name
     assert job_read[2] == job.table_name
-    assert job_read[3] == job.last_compute
+    assert job_read[3] == job.load_connector
+    assert job_read[4] == job.extract_connector
+    assert job_read[6] == job.last_compute
 
 def test_load_job_twice():
     """
     it should be able to upsert a station data
     """
-    job2 = Job(
-        job_id=None,
-        job_name="test_job",
-        table_name="table_test",
-        last_compute=date.today().strftime("%Y-%m-%d, %H:%M:%S")
-    )
+    job2 = copy.deepcopy(job)
+    job2.options = {"new_option": "new"}
     loader.upsert_records(iter([job]))
     loader.upsert_records(iter([job2]))
     records = extract.fetch_data()
     assert len(list(records)) == 1
 
 def test_parse_job():
+    loader.upsert_records(iter([job]))
     records = extract.fetch_data()
     jobs = list(extract.parse_data(records))
     assert len(jobs) == 1
     job_fetch = jobs[0]
+    logger.info(job_fetch)
     assert job_fetch.job_name == job.job_name
     assert job_fetch.table_name == job.table_name
     assert job_fetch.last_compute == job.last_compute
+    assert job_fetch.options == job.options
+    assert job_fetch.load_connector == job.load_connector
+    assert job_fetch.extract_connector == job.extract_connector
