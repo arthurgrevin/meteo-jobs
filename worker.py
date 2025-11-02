46ea65f2584dad0eed8ad5c6c858268c0b7494ea
Arthur <arthur.grevin@gmail.com>
2025-11-03 00:05:34 +0100

    [ADD] Add Route, and Testing Routes

--- worker.py
+++ worker.py
@@ -2,14 +2,9 @@
 
 ## Do EL on this table
 
-from meteo_jobs.extract import Extract
-from meteo_jobs.load import Loader
-from meteo_jobs.connector.postgres import PostgresConnector, PostgresQueriesJob
-from meteo_jobs.action import ActionJob
 from meteo_jobs.logger import get_logger
-from returns.result import Failure, Success
 from fastapi import FastAPI
-from meteo_jobs.server.routes import router
+from meteo_jobs.server.routes_worker import router
 
 import os
 
@@ -36,33 +31,5 @@ if __name__ == "__main__":
 
     import uvicorn
     uvicorn.run("worker:app", host="0.0.0.0", port=8000, reload=True)
-    job_postgres = PostgresQueriesJob()
-    job_postgres_connector = PostgresConnector(
-        host = DB_HOST,
-        port = DB_PORT,
-        dbname = DB_NAME,
-        user = DB_USER,
-        password = DB_PASSWD,
-        db_queries = job_postgres
-    )
-    job_id = 1
-
-    loader = Loader(job_postgres_connector)
-    extract = Extract(job_postgres_connector)
-    getOneJob = ActionJob({"job_id": job_id, "extract": extract})
-    job = getOneJob.execute(iter([]))
-    match job:
-        case Success(jobs):
-            job = next(jobs, None)
-            if job is None:
-                logger.error("Found no job for {job_id}")
-            else:
-                logger.info(job)
-        case Failure(e):
-            logger.error(e)
-        case _:
-            logger.error("No Result")
-
-    loader.close()
 
     logger.info("Extract and Load ends")
